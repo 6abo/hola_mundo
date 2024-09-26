4216c18809d0b29a5fb146e34a0e6668a82dc019
Christian Alvarez <christian.alvarezr@ucuenca.edu.ec>
2024-09-26 16:34:29 -0500

    correctiones

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
-function setup(){
-    createCanvas (windowWidht, windowHeight);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
     frameRate(50);
     background(0);
     rectMode(CENTER);
 }
 
 function draw() {
-    background(20, random(0, 100), 255, 10);
-    strokeWeight(5);
-    noFill();
-    stroke(mouseY, 255, 0);
-    fill(0, 20, 200);
-    circle (mouseX + 80, mouseY, mouseX);
+    background(20, random(0, 100), 255, 10); // Mantiene algo de transparencia
 
+    // Dibujar el círculo
     strokeWeight(5);
-    stroke (0, 255, 0);
+    stroke(mouseY, 255, 0);
+    fill(0, 20, 200); 
+    circle(mouseX + 80, mouseY, mouseX);
+
+    // Dibujar el rectángulo
+    strokeWeight(5);
+    stroke(0, 255, 0);
     fill(100, 200, 20);
     rect(mouseX, mouseY, 100, 100);
 }
